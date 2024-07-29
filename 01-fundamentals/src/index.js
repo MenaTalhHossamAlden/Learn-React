36749564a88bce8b34d9a937a7c22d3eb0aaf811
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-30 02:16:19 +0300

    JSX - CSS

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -26,7 +26,12 @@ const Book = () => {
 const Image = () => <img src='./images/book-1.jpg' alt='Hillbilly Elegy' />;
 const Title = () => <h2>Hillbilly Elegy</h2>;
 const Author = () => {
-  return <h4>J. D. Vance</h4>;
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75',
+    marginTop: '0.5rem',
+  };
+  return <h4 style={inlineHeadingStyles}>J. D. Vance</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
