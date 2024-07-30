41606c5a4ed8d18fa3f58b02c3cde46344bf6004
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-30 22:15:55 +0300

    React Data Flow

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -19,25 +19,26 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = 'shakeAndBake';
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book key={book.id} {...book} />;
+        return <Book key={book.id} {...book} displayValue={displayValue} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props;
-  const displayTitle = () => {
-    console.log(title);
-  };
+  const { img, title, author, displayValue } = props;
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author}</h4>
     </article>
   );
