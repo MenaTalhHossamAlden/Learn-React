fa3b1d4bf0fd05360c755924a7ff64116537aa3a
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-31 17:13:11 +0300

    advanced react initial commit

---
+++ 02-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -0,0 +1,16 @@
+const ErrorExample = () => {
+  let count = 0;
+  const handleClick = () => {
+    count += 1;
+  };
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type="button" onClick={handleClick} className="btn">
+        increase
+      </button>
+    </div>
+  );
+};
+
+export default ErrorExample;
