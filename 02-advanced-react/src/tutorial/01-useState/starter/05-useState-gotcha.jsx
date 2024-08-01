e45aa60bc15f79746385bd9ebe77d30eab878867
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-01 12:15:21 +0300

    05-useState-gotcha

--- 02-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 02-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    setTimeout(() => {
+      console.log('clicked the button');
+      setValue((currentValue) => currentValue + 1);
+    }, 3000);
+  };
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button type="button" onClick={handleClick} className="btn">
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
