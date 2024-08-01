7da5ebb98d2b746ba89bd1bbdaa37d1c102a16a7
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-01 09:25:14 +0300

    03-useState-array

--- 02-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 02-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 
 const UseStateBasics = () => {
-  // console.log(useState(1));
-  // const value = useState('hello')[0];
-  // const func = useState('hello')[1];
-  // console.log(value);
-  // console.log(func);
-
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   const handleClick = () => {
-    // setCount(count + 1);
-    setCount('pants');
+    setCount(count + 1);
   };
 
   return (
