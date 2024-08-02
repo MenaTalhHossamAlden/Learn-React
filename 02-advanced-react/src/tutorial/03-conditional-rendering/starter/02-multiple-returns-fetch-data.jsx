8504d82415adba77de1383d8f6a886a84ce85e35
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-02 04:12:44 +0300

    03-conditional-rendering\02-multiple-returns-fetch-data

--- 02-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 02-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -4,45 +4,44 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnsFetchData = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          setIsError(true);
-          setIsLoading(false);
-          return;
-        }
-        const data = await response.json();
-        setUser(data);
-      } catch (error) {
+  const [user, setUser] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
         setIsError(true);
-        console.log(error);
+        setIsLoading(false);
+        return;
       }
-      setIsLoading(false);
-    };
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      setIsError(true);
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
     fetchData();
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
-
   if (isError) {
     return <h2>There was an error...</h2>;
   }
-
+  const { avatar_url, name, company, bio } = user;
   return (
     <div>
       <img
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
         style={{ width: '150px', borderRadius: '25px' }}
       />
-      <h2>{user.name}</h2>
-      <h4>works at {user.company}</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
     </div>
   );
 };
