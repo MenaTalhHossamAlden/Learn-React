811c626f4f20b8fffa60fd7d8e7656c82b2e6461
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-06 01:30:57 +0300

    Context API

---
+++ 02-advanced-react/src/tutorial/09-context-api/starter/Navbar.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useContext } from 'react';
+import NavLinks from './NavLinks';
+
+export const NavbarContext = createContext();
+
+// custom hook
+
+export const useAppContext = () => {
+  return useContext(NavbarContext);
+};
+
+const Navbar = () => {
+  const [user, setUser] = useState({ name: 'bob' });
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <NavbarContext.Provider value={{ user, logout }}>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks />
+      </nav>
+    </NavbarContext.Provider>
+  );
+};
+export default Navbar;
